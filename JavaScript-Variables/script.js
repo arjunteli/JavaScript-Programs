b7ff67024c38b2fe384fec04c25e0108fb8da0d5
ARJUN TELI <modiarjunsahu1161999@gmail.com>
2023-12-18 13:17:23 +0530

    First Commit

---
+++ JavaScript-Variables/script.js
@@ -0,0 +1,25 @@
+var a = 10;
+var b = 8;
+var c = 'a';
+console.log(c);
+console.log("Sum of a & b = "+(a+b));
+console.log("Type of a = "+ typeof a);
+console.log("Type of b = "+ typeof b);
+console.log("Type of c = "+ typeof c);
+
+let x = 78;
+let y = 78.876;
+let z = "Anamika";
+let d = null;
+let e = undefined;
+console.log(x + " " + y + " " + z+ " " + d+ " " + e);
+console.log(typeof x + " " + typeof y + " " + typeof z+ " " + typeof d+ " " + typeof e);
+
+let o = {
+    name: "Harry",
+    "Job Code" : "567789"
+}
+console.log(o);
+o.salary = "100 Crore";
+console.log(o);
+ x = "567";
---
+++ JavaScript-Variables/script2.js
@@ -0,0 +1,14 @@
+let student = {
+    name: "Harry",
+    "phone number " : "98765432234",
+    "marks ": 99
+
+}
+console.log(  typeof student["phone number "]);
+student["Nationality"] = "Indian";
+console.log(student["Nationality"]);
+
+let a = "String";
+a += 34;
+console.log(a);
+console.log(typeof a)
