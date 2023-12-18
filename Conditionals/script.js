6f3a1f8e3f2da00f416f9cab0a8df68beb1e0aff
ARJUN TELI <modiarjunsahu1161999@gmail.com>
2023-12-18 14:50:32 +0530

    Conditionals Files Added

---
+++ Conditionals/script.js
@@ -0,0 +1,13 @@
+let age = 19;
+
+// if(age>=18){
+//     console.log("You can Drive!!!!");
+// }else{
+//     console.log("You are under age for the driving!!");
+// }
+
+
+// let a = 2**3;//2 power 3
+// console.log(a);
+
+age>18 ? console.log("You can drive the car ") : console.log("You are under age for the driving");
