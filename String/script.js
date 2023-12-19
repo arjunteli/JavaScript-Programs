6e6df3635892df745ee2229935c84c04836ce8a3
ARJUN TELI <modiarjunsahu1161999@gmail.com>
2023-12-19 15:48:39 +0530

    String Files Added

--- String/script.js
+++ String/script.js
@@ -1,1 +1,27 @@
-let real_name = 
+let real_name = "Javascript";
+
+for (let i = 0; i < 12; i++) {
+  console.log(real_name[i]);
+}
+let b = "  Python ";
+
+console.log("*************************************************");
+console.log(real_name + " and " + b + " are programming languages.");
+console.log(`${real_name} and ${b} are programming languages`);//String interpollution language
+//console.log(`We are &{real_name} `)
+
+console.log("rohan\"kai")
+
+console.log(b.toUpperCase())
+console.log(b)
+console.log(b.length)
+console.log(b.slice(0,4))
+console.log(b = b.trim())
+console.log(b)
+
+b  = b + " Program";
+console.log(b)
+console.log(b.replace("o","Z"))
+console.log(b)
+
+console.log(b.concat(" is ", "a ","easy ","langauge"))
