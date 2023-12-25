1b41b9c65c8fba8795765f25a1781163b421b0ee
ARJUN TELI <modiarjunsahu1161999@gmail.com>
2023-12-25 11:10:01 +0530

    >

---
+++ Array/index.js
@@ -0,0 +1,17 @@
+let arr = [1,2,3,4,5];
+console.log(arr);
+console.log(arr.length)
+
+console.log(arr.pop());
+console.log(arr.pop());
+delete arr[2];
+console.log(arr);
+console.log(arr.length)
+console.log(arr[2])
+let aarr2 = [6,7,8,9,10];
+arr = arr.concat(aarr2)
+
+console.log(arr)
+arr.slice(1,6)
+//console.log(arr.slice(1,4))
+console.log(arr)
